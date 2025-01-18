741545e818ca1e470e7292a88162252d324e0ab1
Selin Karaman <selinkaraman02@gmail.com>
2025-01-18 22:55:09 +0300

    Create salary_prediction.py

---
+++ salary_prediction.py
@@ -0,0 +1,72 @@
+### SALARY PREDICTION MODEL - MACHINE LEARNING
+
+## Veri Seti
+
+# Bu veri seti orijinal olarak Carnegie Mellon Üniversitesi'nde bulunan StatLib kütüphanesinden alınmıştır.
+# Veri seti 1988 ASA Grafik Bölümü Poster Oturumu'nda kullanılan verilerin bir parçasıdır.
+# Maaş verileri orijinal olarak Sports Illustrated, 20 Nisan 1987'den alınmıştır.
+# 1986 ve kariyer istatistikleri, Collier Books, Macmillan Publishing Company, New York tarafından
+# yayınlanan 1987 Beyzbol Ansiklopedisi Güncellemesinden elde edilmiştir.
+
+## Değişkenler
+
+# AtBat: 1986-1987 sezonunda bir beyzbol sopası ile topa yapılan vuruş sayısı
+# Hits: 1986-1987 sezonundaki isabet sayısı
+# HmRun: 1986-1987 sezonundaki en değerli vuruş sayısı
+# Runs: 1986-1987 sezonunda takımına kazandırdığı sayı
+# RBI: Bir vurucunun vuruş yaptıgında koşu yaptırdığı oyuncu sayısı
+# Walks: Karşı oyuncuya yaptırılan hata sayısı
+# Years: Oyuncunun major liginde oynama süresi (sene)
+# CAtBat: Oyuncunun kariyeri boyunca topa vurma sayısı
+# CHits: Oyuncunun kariyeri boyunca yaptığı isabetli vuruş sayısı
+# CHmRun: Oyucunun kariyeri boyunca yaptığı en değerli sayısı
+# CRuns: Oyuncunun kariyeri boyunca takımına kazandırdığı sayı
+# CRBI: Oyuncunun kariyeri boyunca koşu yaptırdırdığı oyuncu sayısı
+# CWalks: Oyuncun kariyeri boyunca karşı oyuncuya yaptırdığı hata sayısı
+# League: Oyuncunun sezon sonuna kadar oynadığı ligi gösteren A ve N seviyelerine sahip bir faktör
+# Division: 1986 sonunda oyuncunun oynadığı pozisyonu gösteren E ve W seviyelerine sahip bir faktör
+# PutOuts: Oyun icinde takım arkadaşınla yardımlaşma
+# Assits: 1986-1987 sezonunda oyuncunun yaptığı asist sayısı
+# Errors: 1986-1987 sezonundaki oyuncunun hata sayısı
+# Salary: Oyuncunun 1986-1987 sezonunda aldığı maaş(bin uzerinden)
+# NewLeague: 1987 sezonunun başında oyuncunun ligini gösteren A ve N seviyelerine sahip bir faktör
+
+import numpy as np
+import warnings
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+from sklearn.metrics import mean_squared_error, mean_absolute_error
+from sklearn.preprocessing import StandardScaler, RobustScaler
+from sklearn.model_selection import train_test_split, GridSearchCV, cross_validate, cross_val_score, validation_curve
+from catboost import CatBoostRegressor
+from lightgbm import LGBMRegressor
+from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
+from sklearn.linear_model import LinearRegression, Ridge, Lasso, ElasticNet
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.svm import SVR
+from sklearn.tree import DecisionTreeRegressor
+from xgboost import XGBRegressor
+from sklearn.impute import KNNImputer
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
